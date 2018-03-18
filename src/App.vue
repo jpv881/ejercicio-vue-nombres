61f1a10dc3bcd9c5dddd5b6efae2cd3a3dcfe2ab
Javier Pérez <jpv881@gmail.com>
2018-03-18 23:29:06 +0100

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <avatar v-for="nombre in nombres" :name="nombre"></avatar>
+  </div>
+</template>
+
+<script>
+import avatar from "./components/avatar.vue"
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      nombres: ['juan', 'pepe', 'carlos']
+    }
+  },
+  components: {
+    avatar,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ src/components/avatar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="circle" v-bind:style="{ backgroundColor: color }">{{char}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ["name"],
+  data() {
+    return {
+      char: "",
+      color: this.getRandomColor(),
+    }
+  },
+  methods: {
+    gimmeName(){
+      this.char = this.name.charAt(0);
+    },
+    getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
+  },
+  created(){
+    this.gimmeName();
+  }
+}
+
+</script>
+
+<style>
+  #app div div {
+    padding: 13px;
+    border-radius: 100%;
+    display: block;
+    height: 20px;
+    width: 20px;
+  }
+
+  #app div {
+    text-align: center;
+  }
+
+  .circle {
+    margin-left:auto;
+    margin-right: auto;
+    margin-bottom: 15px;
+  }
+</style>
